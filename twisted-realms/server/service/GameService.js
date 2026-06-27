29876e6af0fb7fa8fae108f91afeb26351b2425b
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-06-27 09:56:00 +0200

    export gameservice

--- twisted-realms/server/service/GameService.js
+++ twisted-realms/server/service/GameService.js
@@ -4,6 +4,8 @@ import User from "../model/User.js";
 class GameService {
   static async checkPlayer(userId, gameId) {
     const players = await Game.getPlayers(gameId);
-    return !players.some(id => Number(id) === Number(userId));
+    return !players.some((id) => Number(id) === Number(userId));
   }
 }
+
+export default GameService;
